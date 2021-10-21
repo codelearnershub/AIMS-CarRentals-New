01464c6aa90150c31c969675dc32738f62a39082
okikiolalawal <okikiolalawal18@gmail.com>
2021-10-21 01:24:31 -1100

    not commit

---
+++ Controllers/SuperAdminDashboardController.cs
@@ -0,0 +1,33 @@
+﻿using AimsCarRentals.Interfaces;
+using AimsCarRentals.Models;
+using AimsCarRentals.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AimsCarRentals.Controllers
+{
+    public class SuperAdminDashboardController : Controller
+    {
+        public readonly IUserRepository _userRepository;
+        public SuperAdminDashboardController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public IActionResult Index()
+        { 
+            int userId = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
+
+            User user = _userRepository.FindUserById(userId);
+
+            SuperAdminDashBoard vm = new SuperAdminDashBoard
+            {
+                User = user
+            };
+            return View();
+        }
+    }
+}
